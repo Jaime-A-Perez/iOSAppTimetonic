b9aba28d87ee7f83b6ed50ecfd5144120846a481
Jaime-A-Perez <82597268+Jaime-A-Perez@users.noreply.github.com>
2024-01-20 16:23:35 -0500

    Add LoginView with welcome message and styling

---
+++ IOSAppTimetonic/IOSAppTimetonic/Views/LoginView.swift
@@ -0,0 +1,29 @@
+//
+//  LoginView.swift
+//  IOSAppTimetonic
+//
+//  Created by Jaime A. Pérez R. on 20/01/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+
+    var body: some View {
+        VStack{
+            Text("Welcome!")
+                .font(.title2)
+                .bold()
+            
+            Text("Timetonic")
+                .font(.largeTitle)
+                .fontWeight(.black)
+            
+            
+        }.padding()
+    }
+}
+
+#Preview {
+    LoginView()
+}
