9e090d44e13b3021eb832ceb466967eed6f429b4
Jaime-A-Perez <82597268+Jaime-A-Perez@users.noreply.github.com>
2024-01-24 10:48:50 -0500

    Enhance MockURLProtocol to handle error scenarios and HTTP responses

--- IOSAppTimetonic/IOSAppTimetonicTests/ServicesTests/MockURLProtocol.swift
+++ IOSAppTimetonic/IOSAppTimetonicTests/ServicesTests/MockURLProtocol.swift
@@ -9,33 +9,49 @@ import Foundation
 
 // To simulate network responses
 class MockURLProtocol: URLProtocol {
-    // It is used to store the data that will be returned as part of the simulated response
+    // Data to be returned as part of the simulated response
     static var mockData: Data?
     
-    // Overrides the class method 'canInit(with:)'. This method determines whether this protocol can handle the given request
+    // Error to be returned as part of the simulated response
+    static var mockError: NSError?
+
+    // HTTP Response to be returned as part of the simulated response
+    static var mockResponse: HTTPURLResponse?
+
+    // This method determines whether this protocol can handle the given request
     override class func canInit(with request: URLRequest) -> Bool {
         // Here, it's set to always return true, indicating that this protocol can handle all requests
         return true
     }
     
-    // Overrides the class method 'canonicalRequest(for:)'. This method returns a canonical version of the request
+    // This method returns a canonical version of the request
     override class func canonicalRequest(for request: URLRequest) -> URLRequest {
-        // In this implementation, it simply returns the original request without modification
+        // It simply returns the original request without modification
         return request
     }
     
-    // Overrides the instance method 'startLoading'. This method starts loading the request
+    // This method starts loading the request
     override func startLoading() {
-        // Here, it checks if mock data is available, and if so, sends it to the client as a response
-        if let data = MockURLProtocol.mockData {
-            self.client?.urlProtocol(self, didLoad: data)
+        if let error = MockURLProtocol.mockError {
+            // Simulate an error scenario
+            self.client?.urlProtocol(self, didFailWithError: error)
+        } else {
+            if let response = MockURLProtocol.mockResponse {
+                // Send a custom HTTP response if available
+                self.client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
+
+            if let data = MockURLProtocol.mockData {
+                // Send the mock data
+                self.client?.urlProtocol(self, didLoad: data)
+            }
         }
-        // It then notifies the client that loading has finished
+
+        // Notify the client that loading has finished
         self.client?.urlProtocolDidFinishLoading(self)
     }
     
-    //  This method stops loading the request
     override func stopLoading() {
-        // It's empty in this case, as there's no actual network request to cancel
+        // No actual network request to cancel, but this is where you would clean up if there was
     }
 }
