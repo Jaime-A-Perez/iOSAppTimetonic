4134776ac35aada29a58311e9f3ae3efd95fc6d8
Jaime-A-Perez <82597268+Jaime-A-Perez@users.noreply.github.com>
2024-02-09 20:58:38 -0500

    Refactor AuthVerificationViewModel to: Update sessKeyDelete method to reset credentials and auth state. Add keyManagementError

--- IOSAppTimetonic/IOSAppTimetonic/ViewModels/AuthVerificationViewModels/AuthVerificationViewModel.swift
+++ IOSAppTimetonic/IOSAppTimetonic/ViewModels/AuthVerificationViewModels/AuthVerificationViewModel.swift
@@ -15,9 +15,10 @@ class AuthVerificationViewModel : ObservableObject {
     // Properties related to authentication state
     @Published private(set) var latestErrorMessage = ""
     @Published private(set) var isAuthenticating = true
+    @Published private(set) var authenticationIndicator: Int = 0
+    @Published var keyManagementError = ""
     @Published var isAuthenticationComplete = false
     @Published var authenticationStatusMessage = ""
-    @Published private(set) var authenticationIndicator: Int = 0
     @Published var authenticationState : AuthenticationState = .checkingIfAlreadyAuthenticated
     
        
@@ -145,40 +146,52 @@ class AuthVerificationViewModel : ObservableObject {
     // MARK: - Token Management
     
     /// Stores a key in the keychain for a given account type
-    func storeKey(_ key: String, forAccount accountType: String) {
+    private func storeKey(_ key: String, forAccount accountType: String) {
         do {
             let tokenCreator = TokenCreator(keychainService: KeychainService())
             try tokenCreator.saveToken(key, service: Constants.API.baseUrl, account: accountType)
         } catch {
-            self.latestErrorMessage = "Failed to store token: \(error)"
+            self.keyManagementError = "Failed to store token: \(error)"
         }
     }
     
     /// Deletes the session key from the keychain
     func sessKeyDelete() -> Bool {
         do {
+            loginViewModel.user.email = ""
+            loginViewModel.user.password = ""
+            Constants.API.userEmail = ""
+            userEmail = ""
+            userPwd = ""
+            isAuthenticating = true
+            authenticationIndicator = 0
+            keyManagementError = ""
+            isAuthenticationComplete = false
+            authenticationStatusMessage = ""
+            authenticationState = .idle
+            loginViewModel.isActiveAuthView = false
             let deleter = TokenDeleter(keychainService: KeychainService())
             let _ = try deleter.deleteToken(service: Constants.API.baseUrl, account: "Sessk")
             let _ = try deleter.deleteToken(service: Constants.API.baseUrl, account: "o_u")
             return true
         }catch {
-            self.latestErrorMessage = "Could not delete stored key: \(error)"
+            self.keyManagementError = "Could not delete stored key: \(error)"
             return false
         }
     }
     
-    
+
     // MARK: - Authentication Status
     
     /// Verifies whether the necessary keys are stored in the keychain
-    func verifyKeyStored() -> Bool {
+    private func verifyKeyStored() -> Bool {
         do {
             let tokenReader = TokenReader(keychainService: KeychainService())
             let k1 = try tokenReader.retrieveToken(service: Constants.API.baseUrl, account: "Sessk")
             let k2 = try tokenReader.retrieveToken(service: Constants.API.baseUrl, account: "o_u")
             if (k1 != "") && (k2 != "") {return true} else { return false}
         } catch {
-            self.latestErrorMessage = "Could not verify stored key: \(error)"
+            self.keyManagementError = "Could not verify stored key: \(error)"
             return false
         }
     }
